a5b315d2eefffbe8e2a48fbef8d96fe7f0b6e91f
Dheyaa <deaa86_arch@hotmail.com>
2016-10-09 15:22:08 +1100

    third

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Admin.create email: 'deaa86_arch@hotmail.com', password: 'xfad1ani2'
